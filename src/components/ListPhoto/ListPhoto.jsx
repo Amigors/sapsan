b1b83331ff4903e3351ef94ad77a0e03a363e426
Lubinets Sergey Nikolaevich <snlubinets@skr.local>
2024-06-11 19:00:19 +0300

    pagination, modal

--- src/components/ListPhoto/ListPhoto.jsx
+++ src/components/ListPhoto/ListPhoto.jsx
@@ -1,14 +1,29 @@
-import style from './ListPhoto.module.scss'
+import { useState } from "react";
+import style from "./ListPhoto.module.scss";
+import ReactModal from "react-modal";
 
 const ListPhoto = ({ image, key }) => {
-    console.log('image', image)
-    return (
-        <>
-            <div className={style.photoBlock}>
-                <img className={style.photo} src={image.urls.regular} />
-            </div>
-        </>
-    )
-}
+  const [showModal, setShowModal] = useState(false);
 
-export default ListPhoto
+  const handleModal = () => {
+    setShowModal(!showModal);
+  };
+  return (
+    <>
+      <div className={style.photoBlock} onClick={handleModal}>
+        <img className={style.photo} src={image.urls.regular} />
+      </div>
+
+      <ReactModal
+        isOpen={showModal}
+        style={{ overlay: { backgroundColor: "#000000", opacity: "30%" } }}
+        onRequestClose={handleModal}
+        shouldCloseOnOverlayClick={true}
+      >
+        <img className={style.photoModal} src={image.urls.full} />
+      </ReactModal>
+    </>
+  );
+};
+
+export default ListPhoto;
--- src/components/Searching/Searching.jsx
+++ src/components/Searching/Searching.jsx
@@ -1,47 +1,70 @@
-import { useRef } from "react"
-import { useState } from "react"
-import ListPhoto from "../ListPhoto/ListPhoto"
-import axios from "axios"
-import { url } from "../../api/api"
-import style from './Searching.module.scss'
+import { useRef } from "react";
+import { useState } from "react";
+import ListPhoto from "../ListPhoto/ListPhoto";
+import axios from "axios";
+import { url } from "../../api/api";
+import style from "./Searching.module.scss";
+import { Oval } from "react-loader-spinner";
 
 const Searching = () => {
-    const [images, setImages] = useState([])
-    const ref = useRef(null)
-    console.log(ref)
+  const [images, setImages] = useState([]);
+  const [total, setTotal] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const ref = useRef(null);
 
-    const handleClick = async () => {
-        try {
-            const response = await axios.get(url + `${ref.current.value}`)
-            if (response.data.total_pages > 1) {
-                for (let i = 1; i <= response.data.total_pages; i++) {
-                    const resp = await axios.get(url + `${ref.current.value}` + '&page=' + `${i}`)
-                    setImages(resp.data)
-                }
-            } else if (response.data.total_pages === 1) {
-
-            }
-
-        } catch (error) {
-            console.log(error.message);
+  const handleClick = async () => {
+    try {
+      setImages([]);
+      setTotal(null);
+      setIsLoading(true);
+      const response = await axios.get(url + `${ref.current.value}`);
+      setTotal(response.data.total);
+      if (response.data.total_pages > 1) {
+        for (let i = 1; i <= response.data.total_pages; i++) {
+          const resp = await axios.get(
+            url + `${ref.current.value}` + "&page=" + `${i}`
+          );
+          setImages((images) => images.concat(resp.data.results));
         }
+      } else if (response.data.total_pages === 1) {
+        setImages(response.data.results);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.message);
+      setIsLoading(false);
     }
+  };
 
-    return (
-        <>
-            <div className={images.total > 0 ? style.searchBlock : style.searchBlock__empty}>
-                <input className={style.searchBlock__input} ref={ref} type="text" placeholder="Телефоны, яблоки, груши..." />
-                <button className={style.searchBlock__button} type="submit" onClick={handleClick}><span className={style.searchBlock__buttonText}>Искать</span></button>
-            </div>
-            {console.log(' 29 str', images)}
-            <div className={style.photosBlock}>
-                {images.total > 0 && images.results.map((image, index) => {
-                    return <ListPhoto image={image} key={index} />
-                })
-                }
-            </div>
-        </>
-    )
-}
+  console.log(total);
+  return (
+    <>
+      <div className={total > 0 ? style.searchBlock : style.searchBlock__empty}>
+        <input
+          className={style.searchBlock__input}
+          ref={ref}
+          type="text"
+          placeholder="Телефоны, яблоки, груши..."
+        />
+        <button
+          className={style.searchBlock__button}
+          type="submit"
+          onClick={handleClick}
+        >
+          <span className={style.searchBlock__buttonText}>Искать</span>
+        </button>
+      </div>
+      {console.log(" 29 str", images)}
 
-export default Searching
+      <div className={style.photosBlock}>
+        {total > 0 &&
+          images.map((image, index) => {
+            return <ListPhoto image={image} key={index} />;
+          })}
+        {isLoading && <Oval />}
+      </div>
+    </>
+  );
+};
+
+export default Searching;
