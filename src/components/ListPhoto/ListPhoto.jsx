b0a3ceba6dc24a80c9d3012be923c1b27bbe1360
Lubinets Sergey <lubinets.sn@yndex.ru>
2024-06-13 14:54:14 +0300

    mobile style fix

--- src/components/ListPhoto/ListPhoto.jsx
+++ src/components/ListPhoto/ListPhoto.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import style from "./ListPhoto.module.scss";
 import ReactModal from "react-modal";
 import { IoIosClose } from "react-icons/io";
+import { createPortal } from "react-dom";
 
 const ListPhoto = ({ image, key }) => {
   const [showModal, setShowModal] = useState(false);
@@ -15,16 +16,19 @@ const ListPhoto = ({ image, key }) => {
         <img key={key} className={style.photo} src={image.urls.regular} alt="photo" />
       </div>
 
-      <ReactModal
-        isOpen={showModal}
-        className={style.photoModalBlock}
-        onRequestClose={handleModal}
-        shouldCloseOnOverlayClick={true}
-        style={{ overlay: { backgroundColor: 'rgba(0,0,0,0.3)' } }}
-      >
-        <IoIosClose className={style.closeButton} onClick={handleModal} />
-        <img key={key} className={style.photoModal} onClick={handleModal} src={image.urls.full} alt="photo" />
-      </ReactModal>
+      {createPortal(
+        <ReactModal
+          isOpen={showModal}
+          className={style.photoModalBlock}
+          onRequestClose={handleModal}
+          shouldCloseOnOverlayClick={true}
+          ariaHideApp={false}
+          style={{ overlay: { backgroundColor: 'rgba(0,0,0,0.3)' } }}
+        >
+          <IoIosClose className={style.closeButton} onClick={handleModal} />
+          <img key={key} className={style.photoModal} onClick={handleModal} src={image.urls.full} alt="photo" />
+        </ReactModal>, document.body)
+      }
     </>
   );
 };
