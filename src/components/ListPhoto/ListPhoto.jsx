542609908090e57fcb8c5e01a1d899fe8a8a2586
Lubinets Sergey <lubinets.sn@yndex.ru>
2024-06-13 12:14:24 +0300

    mob vers

--- src/components/ListPhoto/ListPhoto.jsx
+++ src/components/ListPhoto/ListPhoto.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import style from "./ListPhoto.module.scss";
 import ReactModal from "react-modal";
+import { IoIosClose } from "react-icons/io";
 
 const ListPhoto = ({ image, key }) => {
   const [showModal, setShowModal] = useState(false);
@@ -11,16 +12,18 @@ const ListPhoto = ({ image, key }) => {
   return (
     <>
       <div className={style.photoBlock} onClick={handleModal}>
-        <img className={style.photo} src={image.urls.regular} />
+        <img key={key} className={style.photo} src={image.urls.regular} alt="photo" />
       </div>
 
       <ReactModal
         isOpen={showModal}
-        style={{ overlay: { backgroundColor: "#000000", opacity: "30%" } }}
+        className={style.photoModalBlock}
         onRequestClose={handleModal}
         shouldCloseOnOverlayClick={true}
+        style={{ overlay: { backgroundColor: 'rgba(0,0,0,0.3)' } }}
       >
-        <img className={style.photoModal} src={image.urls.full} />
+        <IoIosClose className={style.closeButton} onClick={handleModal} />
+        <img key={key} className={style.photoModal} onClick={handleModal} src={image.urls.full} alt="photo" />
       </ReactModal>
     </>
   );
--- src/components/Searching/Searching.jsx
+++ src/components/Searching/Searching.jsx
@@ -5,66 +5,92 @@ import axios from "axios";
 import { url } from "../../api/api";
 import style from "./Searching.module.scss";
 import { Oval } from "react-loader-spinner";
+import { CiSearch } from "react-icons/ci";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const Searching = () => {
-  const [images, setImages] = useState([]);
-  const [total, setTotal] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const ref = useRef(null);
+    const [images, setImages] = useState([]);
+    const [total, setTotal] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isFetched, setIsFetched] = useState(false)
+    const [showInputClose, setShowInputClose] = useState(false)
+    const ref = useRef(null);
 
-  const handleClick = async () => {
-    try {
-      setImages([]);
-      setTotal(null);
-      setIsLoading(true);
-      const response = await axios.get(url + `${ref.current.value}`);
-      setTotal(response.data.total);
-      if (response.data.total_pages > 1) {
-        for (let i = 1; i <= response.data.total_pages; i++) {
-          const resp = await axios.get(
-            url + `${ref.current.value}` + "&page=" + `${i}`
-          );
-          setImages((images) => images.concat(resp.data.results));
+    const handleClick = async () => {
+        try {
+            setImages([]);
+            setTotal(null);
+            setIsLoading(true);
+            setIsFetched(true)
+            const response = await axios.get(url + `${ref.current.value}`);
+            setTotal(response.data.total);
+            if (response.data.total_pages > 1) {
+                for (let i = 1; i <= response.data.total_pages; i++) {
+                    const resp = await axios.get(
+                        url + `${ref.current.value}` + "&page=" + `${i}`
+                    );
+                    setImages((images) => images.concat(resp.data.results));
+                }
+            } else if (response.data.total_pages === 1) {
+                setImages(response.data.results);
+            }
+            setIsLoading(false);
+        } catch (error) {
+            console.log(error.message);
+            setIsLoading(false);
         }
-      } else if (response.data.total_pages === 1) {
-        setImages(response.data.results);
-      }
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error.message);
-      setIsLoading(false);
+    };
+
+    const changeInput = () => {
+        if (ref.current.value) {
+            setShowInputClose(true)
+        } else {
+            setShowInputClose(false)
+        }
     }
-  };
+    const handleCloseIcon = () => {
+        setShowInputClose(false)
+        ref.current.value = null
+    }
 
-  console.log(total);
-  return (
-    <>
-      <div className={total > 0 ? style.searchBlock : style.searchBlock__empty}>
-        <input
-          className={style.searchBlock__input}
-          ref={ref}
-          type="text"
-          placeholder="Телефоны, яблоки, груши..."
-        />
-        <button
-          className={style.searchBlock__button}
-          type="submit"
-          onClick={handleClick}
-        >
-          <span className={style.searchBlock__buttonText}>Искать</span>
-        </button>
-      </div>
-      {console.log(" 29 str", images)}
+    return (
+        <>
+            <div className={isFetched ? style.searchBlock : style.searchBlock__empty}>
+                <div className={style.searchBlock__input}>
+                    <i className={style.searchBlock__searchIcon} aria-hidden='true'><CiSearch /></i>
+                    <input
+                        className={style.searchBlock__inputField}
+                        ref={ref}
+                        type="text"
+                        onChange={changeInput}
+                        placeholder="Телефоны, яблоки, груши..."
+                    />
+                    {showInputClose && <i className={style.searchBlock__closeIcon} onClick={handleCloseIcon}><IoMdCloseCircle /></i>}
 
-      <div className={style.photosBlock}>
-        {total > 0 &&
-          images.map((image, index) => {
-            return <ListPhoto image={image} key={index} />;
-          })}
-        {isLoading && <Oval />}
-      </div>
-    </>
-  );
+                </div>
+                <button
+                    className={style.searchBlock__button}
+                    type="submit"
+                    onClick={handleClick}
+                >
+                    <span className={style.searchBlock__buttonText}>Искать</span>
+                </button>
+            </div>
+            <div className={style.photosBlock}>
+                {isFetched &&
+                    (total > 0 ?
+                        (images.map((image, index) => {
+                            return <ListPhoto image={image} key={index} />;
+                        }))
+                        :
+                        (
+                            <div className={style.emptyFetch}>К сожалению поиск не дал результатов</div>
+                        ))
+                }
+                {isLoading && <Oval />}
+            </div>
+        </>
+    );
 };
 
 export default Searching;
