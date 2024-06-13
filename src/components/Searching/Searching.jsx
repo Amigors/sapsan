385fe76dd1228395a4aa32464588a2b8458d6b4a
Lubinets Sergey <lubinets.sn@yndex.ru>
2024-06-13 14:33:24 +0300

    infinite scroll

--- src/components/Searching/Searching.jsx
+++ src/components/Searching/Searching.jsx
@@ -1,5 +1,5 @@
-import { useRef } from "react";
-import { useState } from "react";
+import React, { useState, useRef, useCallback } from 'react';
+import useInfiniteScroll from 'react-infinite-scroll-hook';
 import ListPhoto from "../ListPhoto/ListPhoto";
 import axios from "axios";
 import { url } from "../../api/api";
@@ -12,46 +12,52 @@ const Searching = () => {
     const [images, setImages] = useState([]);
     const [total, setTotal] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
-    const [isFetched, setIsFetched] = useState(false)
-    const [showInputClose, setShowInputClose] = useState(false)
+    const [isFetched, setIsFetched] = useState(false);
+    const [showInputClose, setShowInputClose] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
     const ref = useRef(null);
 
-    const handleClick = async () => {
+    const fetchData = useCallback(async () => {
+        setIsLoading(true);
         try {
-            setImages([]);
-            setTotal(null);
-            setIsLoading(true);
-            setIsFetched(true)
-            const response = await axios.get(url + `${ref.current.value}`);
+            const response = await axios.get(`${url}${ref.current.value}&page=${currentPage}`);
             setTotal(response.data.total);
-            if (response.data.total_pages > 1) {
-                for (let i = 1; i < response.data.total_pages; i++) {
-                    const resp = await axios.get(
-                        url + `${ref.current.value}` + "&page=" + `${i}`
-                    );
-                    setImages((images) => images.concat(resp.data.results));
-                }
-            } else if (response.data.total_pages === 1) {
-                setImages(response.data.results);
-            }
-            setIsLoading(false);
+            setImages(prevImages => [...prevImages, ...response.data.results]);
+            setCurrentPage(currentPage + 1);
         } catch (error) {
-            console.log(error.message);
+            console.error(error.message);
+        } finally {
             setIsLoading(false);
         }
+    }, [currentPage]);
+
+    const [scrollRef] = useInfiniteScroll({
+        loading: isLoading,
+        hasNextPage: currentPage <= total,
+        onLoadMore: fetchData,
+        disabled: !isFetched,
+    })
+
+    const handleClick = () => {
+        setIsFetched(true);
+        setImages([]);
+        setCurrentPage(1);
+        fetchData();
     };
 
     const changeInput = () => {
         if (ref.current.value) {
-            setShowInputClose(true)
+            setShowInputClose(true);
         } else {
-            setShowInputClose(false)
+            setShowInputClose(false);
         }
-    }
+    };
+
     const handleCloseIcon = () => {
-        setShowInputClose(false)
-        ref.current.value = null
-    }
+        setShowInputClose(false);
+        ref.current.value = '';
+    };
+
 
     return (
         <>
@@ -76,18 +82,12 @@ const Searching = () => {
                     <span className={style.searchBlock__buttonText}>Искать</span>
                 </button>
             </div>
-            <div className={style.photosBlock}>
-                {isFetched &&
-                    (total > 0 ?
-                        (images.map((image, index) => {
-                            return <ListPhoto image={image} key={index} />;
-                        }))
-                        :
-                        (
-                            <div className={style.emptyFetch}>К сожалению поиск не дал результатов</div>
-                        ))
-                }
+            <div className={style.photosBlock} ref={scrollRef}>
+                {isFetched && total > 0 && images.map((image, index) => (
+                    <ListPhoto image={image} key={index} />
+                ))}
                 {isLoading && <Oval />}
+                {!isLoading && total === 0 && <div className={style.emptyFetch}>К сожалению поиск не дал результатов</div>}
             </div>
         </>
     );
